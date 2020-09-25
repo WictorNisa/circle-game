d794efb032a20cf341035c9ae1009ff3d682f1f0
WictorNisa <wictornisa@hotmail.com>
2020-09-25 09:16:57 +0200

    Finished basic game features. Adding level feature next

--- sketch.js
+++ sketch.js
@@ -1,14 +1,33 @@
 //Global variables
-let circleRadius = 200 * Math.random() + 5;
+let circleRadius = 100;
 let x, y;
+let score = 0;
+let r, g, b;
+  
 
+// Makes an function to see if the user has clicked on the circle or not
+function mousePressed() {
+  //calculate the distance between the x, y location of the mouse and the x, y location of the circle
+  let d = dist(mouseX, mouseY, x, y);
 
-//Makes a random rbg string for diffrent colors on the circle each time it gets spawned
-let r = Math.floor(Math.random() * (255 - 0 + 1) + 0);
-let g = Math.floor(Math.random() * (255 - 0 + 1) + 0);
-let b = Math.floor(Math.random() * (255 - 0 + 1) + 0);
-let rgbString = r + ", " + g + ", " + b;
+  //Checks to see if the distance stored in 'd' is less than the radius of the circle
+  if (d < circleRadius) {
+    // Assigns new random values to X and Y so the circle can move around when the user clicks it
+    x = random(windowWidth);
+    y = random(windowHeight);
+    //Adds one 'point' to the variable score if the user clicks the circle
+    score++;
+    //Changes the radius of the circle to random when it's clicked
+    circleRadius = Math.floor( Math.random() * 300);
 
+    // Changes the RBG value of the circle when clicked to a new random RBG value
+    r = random(255);
+    g = random(255);
+    b = random(255);
+
+    
+  }
+}
 
 
 // Set-ups the canvas
@@ -18,14 +37,26 @@ function setup() {
   // Return a random floating-point number inside windowHeight and windowWidth and assigns it to the variable X and Y 
   x = random(windowWidth);
   y = random(windowHeight);
+
+   r = random(255);
+   g = random(255);
+   b = random(255);
+
+   
 }
 
 function draw() {
   background(192, 188, 181);
 
 
-  //Draw elements
-  fill(rgbString);
+  //Draws the circle. Here Im using the random color generator
   noStroke();
+  fill(r , g , b);
   circle(x, y, circleRadius);
+
+
+  textSize(30);
+  text('Your Score: ' + score, 10, 30);
+
+
 }
